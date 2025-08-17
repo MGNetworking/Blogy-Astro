220e8b8f62ef7921512c2701d0da0070d17bf4ae
Maxime <ghalem.maxime@gmail.com>
2025-08-17 17:26:06 +0200

    Add config static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   // Configuration de base
+  output: "static",
   site: "https://mgnetworking.github.io/Blogy-Astro", // Remplace par ton URL
   base: "/Blogy-Astro", // Base path
 
